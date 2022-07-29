0b0b17d832c0ce33381c4d34a925a27bb1d45113
David100mark <37455012+David100mark@users.noreply.github.com>
2022-07-29 20:08:52 +0200

    Update fan_control.py
    use pigpio instead because it supports hardware_PWM

--- fan_control.py
+++ fan_control.py
@@ -1,57 +1,51 @@
 #! /usr/bin/env python3
-import RPi.GPIO as GPIO
+
+import pigpio
 import time
 import signal
 import sys
 
-# The Noctua PWM control actually wants 25 kHz (kilo!), see page 6 on:
-# https://noctua.at/pub/media/wysiwyg/Noctua_PWM_specifications_white_paper.pdf
-# However, the RPi.GPIO library causes high CPU usage when using high
-# frequencies - probably because it can currently only do software PWM.
-# So we set a lower frequency in the 10s of Hz here. You should expect that
-# this value doesn't work very well and adapt it to what works in your setup.
-# We will work on the issue and try to use hardware PWM in the future:
-PWM_FREQ = 25           # [Hz] PWM frequency
+pi = pigpio.pi()
 
-FAN_PIN = 18            # BCM pin used to drive PWM fan
-WAIT_TIME = 1           # [s] Time to wait between each refresh
+PWM_FREQ = 25000        # 25kHZ
 
-OFF_TEMP = 40           # [°C] temperature below which to stop the fan
-MIN_TEMP = 45           # [°C] temperature above which to start the fan
-MAX_TEMP = 70           # [°C] temperature at which to operate at max fan speed
-FAN_LOW = 1
-FAN_HIGH = 100
-FAN_OFF = 0
-FAN_MAX = 100
+FAN_PIN = 18            #
+
+WAIT_TIME = 1           # [s] time between refreshs
+
+OFF_TEMP = 40           # [C] temp below fan off
+MIN_TEMP = 45           # [C] temp above fan start
+MAX_TEMP = 70           # [C] temp above fan at max
+FAN_LOW = 20            # [%] fan speed
+FAN_OFF = 0             # [%] fan speed
+FAN_HIGH = 100          # [%] fan speed
+
 FAN_GAIN = float(FAN_HIGH - FAN_LOW) / float(MAX_TEMP - MIN_TEMP)
 
+def getCpuTemperature ():
+        with open('/sys/class/thermal/thermal_zone0/temp') as f:
+                return float(f.read()) / 1000
 
-def getCpuTemperature():
-    with open('/sys/class/thermal/thermal_zone0/temp') as f:
-        return float(f.read()) / 1000
+def controlFan(frequency):
+        pi.hardware_PWM(FAN_PIN, PWM_FREQ, frequency )
 
 
-def handleFanSpeed(fan, temperature):
-    if temperature > MIN_TEMP:
-        delta = min(temperature, MAX_TEMP) - MIN_TEMP
-        fan.start(FAN_LOW + delta * FAN_GAIN)
-
-    elif temperature < OFF_TEMP:
-        fan.start(FAN_OFF)
-
+def handleFanSpeed(temperature):
+        if temperature >= MAX_TEMP:
+                controlFan(FAN_HIGH * 10000)
+        elif temperature >= MIN_TEMP:
+                delta = temperature - MIN_TEMP
+                fanspeed = int(FAN_LOW * 10000 + delta * FAN_GAIN * 10000)
+                controlFan(fanspeed)
+        elif temperature < OFF_TEMP:
+                controlFan(FAN_OFF)
 
 try:
-    signal.signal(signal.SIGTERM, lambda *args: sys.exit(0))
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(FAN_PIN, GPIO.OUT, initial=GPIO.LOW)
-    fan = GPIO.PWM(FAN_PIN, PWM_FREQ)
-    while True:
-        handleFanSpeed(fan, getCpuTemperature())
-        time.sleep(WAIT_TIME)
+
+        while True:
+                handleFanSpeed(getCpuTemperature())
+                time.sleep(WAIT_TIME)
 
 except KeyboardInterrupt:
-    pass
-
-finally:
-    GPIO.cleanup()
+        controlFan(FAN_HIGH *  10000)
+        pi.stop()
